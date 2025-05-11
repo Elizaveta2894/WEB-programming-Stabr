1c8bf0877211d003f9a3b207591249c14315299b
Стабровская Елизавета <liza.stabrovskaya2894@gmail.com>
2025-05-11 13:10:01 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books =[
+    {
+        author: 'Лао-цзы',
+        name:'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 360, 
+        bestseller: false, 
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name:'Космос',
+        publicationYear: 2024,
+        price: 400, 
+        bestseller: true, 
+        available: false
+    },
+    {
+        author: 'Баладжи Касал',
+        name:'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 460, 
+        bestseller: true, 
+        available: true
+    },
+    { 
+        author: "Джейн Остин", 
+        name: "Гордость и предубеждение", 
+        publicationYear: 1813, 
+        price: 550, 
+        bestseller: true, 
+        available: true 
+    },
+    { 
+        author: "Джордж Оруэлл", 
+        name: "1984", 
+        publicationYear: 1949, 
+        price: 600, 
+        bestseller: true, 
+        available: true 
+    },
+    { 
+        author: "Харпер Ли", 
+        name: "Убить пересмешника", 
+        publicationYear: 1960, 
+        price: 480, 
+        bestseller: true, 
+        available: true 
+    },
+    { 
+        author: "Ф. Скотт Фицджеральд", 
+        name: "Великий Гэтсби", 
+        publicationYear: 1925, 
+        price: 520, 
+        bestseller: false, 
+        available: true 
+    },
+    { 
+        author: "Габриэль Гарсиа Маркес", 
+        name: "Сто лет одиночества", 
+        publicationYear: 1967, 
+        price: 700, 
+        bestseller: true, 
+        available: false 
+    },       
+    { 
+        author: "Лев Толстой", 
+        name: "Анна Каренина", 
+        publicationYear: 1877, 
+        price: 800, 
+        bestseller: false, 
+        available: true 
+    },
+    { 
+        author: "Мигель де Сервантес", 
+        name: "Дон Кихот", 
+        publicationYear: 1605, 
+        price: 650, 
+        bestseller: false, 
+        available: false 
+    },
+];
